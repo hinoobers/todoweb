c02e3f21b93c53747af93c10806a9a4d96015536
hinoob <reio.poldmaa@voco.ee>
2024-09-09 10:16:38 +0300

    Read data from file

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const fs = require("fs");
 
 app.set("view engine", 'ejs');
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
-    res.render("index");
+    fs.readFile("./tasks", 'utf8', (err, data) => {
+        if(err) {
+            console.error(err);
+            return;
+        }
+        res.render("index", {tasks: data.split("\n")});
+    })
 });
 
 app.listen(8080, () => {
