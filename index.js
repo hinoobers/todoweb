a652d61a101a59941e5f55dd05b0f90ae7a50ab8
hinoob <reio.poldmaa@voco.ee>
2024-09-09 10:34:29 +0300

    Write data to file

--- index.js
+++ index.js
@@ -6,14 +6,42 @@ const fs = require("fs");
 app.set("view engine", 'ejs');
 app.set("views", path.join(__dirname, "views"));
 
+const readFile = (filename) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile("./" + filename, 'utf8', (err, data) => {
+            if(err) {
+                console.error(err);
+                reject("Error!");
+            }
+
+            resolve(data.split("\n"));
+        });
+    });
+};
+
 app.get("/", (req, res) => {
-    fs.readFile("./tasks", 'utf8', (err, data) => {
-        if(err) {
-            console.error(err);
-            return;
-        }
-        res.render("index", {tasks: data.split("\n")});
-    })
+    readFile("tasks").then(tasks => {
+        res.render("index", {tasks});
+    });
+});
+
+app.use(express.urlencoded({extended: true}));
+
+app.post("/", (req, res) => {
+    console.log("Form sent data", req.body);
+    readFile("tasks").then(tasks => {
+        tasks.push(req.body.task);
+
+        const data = tasks.join("\n");
+        fs.writeFile("./tasks", data, err => {
+            if(err) {
+                console.error(err);
+                return;
+            }
+
+            res.redirect("/");
+        });
+    });
 });
 
 app.listen(8080, () => {
